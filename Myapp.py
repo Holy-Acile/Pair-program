ef62dd858db2bec5e4b05ab80fd58df3b175a9e8
Holy-Acile <huanahuan0202@163.com>
2020-10-04 23:18:17 +0800

    finish

--- Myapp.py
+++ Myapp.py
@@ -1,32 +1,35 @@
 #all ok!!！
-
+import os
 import re
 from fractions import Fraction
 import sys
 
 from module.cal_problem_list import cal_problem_list
 from module.gen_problem_list import gen_problem_list
-
+from module.check_txt import load,check
 
 # 主动抛出异常
 def raise_ex(s1, s2):
     if (s1 == '-n' and s2 == '-r'):
-        return True
-    ex = Exception('Error:参数格式错误！')
-    raise ex
-
-
-def main():
-    try:
-        s1, n, s2, r = sys.argv[1:5]
-    except BaseException:
-        print("Error: 输入命令错误 \n正确格式为-n num -r num \n")
+        return 1
+    elif (s1 == '-e' and s2 == '-a'):
+        return 2
     else:
-        try:
-            print(raise_ex(s1, s2))
-        except Exception as re:
-            print(re)
-        else:
+     ex = Exception('Error:参数格式错误！')
+     raise ex
+
+
+def making_problem(n,r):
+    # try:
+    #     s1, n, s2, r = sys.argv[1:5]
+    # except BaseException:
+    #     print("Error: 输入命令错误 \n正确格式为-n num -r num \n")
+    # else:
+    #     try:
+    #         print(raise_ex(s1, s2))
+    #     except Exception as re:
+    #         print(re)
+    #     else:
             try:
                 int(n)
                 int(r)
@@ -41,6 +44,7 @@ def main():
                 problem = gen_problem_list(n, r)
                 print(problem)
                 answer = cal_problem_list(problem)
+
                 for i in range(len(problem)):
                     p = str(problem[i])
                     p = str(i + 1) + '. ' + p + ' = ' + '\n'
@@ -54,6 +58,30 @@ def main():
                 Answ.close()
                 print(answer)
 
+def main():
+    try:
+        s1, n, s2, r = sys.argv[1:5]
+    except BaseException:
+        print("Error: 输入命令错误")# \n正确格式为-n num -r num \n
+    else:
+        try:
+            res=raise_ex(s1, s2)
+        except Exception as re:
+            print(re)
+        else:
+            if (res==1):
+                making_problem(n,r)
+            elif (res == 2):
+                e = os.getcwd() +'\\'+ n
+                a = os.getcwd() +'\\'+ r
+                load_e = load(e)
+                load_a = load(a)
+                check(load_e, load_a)
+            else:
+                print('Error:选择错误')
 
+
+
+    # print(str)
 if __name__ == '__main__':
     main()
