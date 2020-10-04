2a92c558e06154310e31281a22ee02e1d2f54160
MrEdge <mredge123@163.com>
2020-10-04 23:41:05 +0800

    modify the code format

--- Myapp.py
+++ Myapp.py
@@ -6,7 +6,8 @@ import sys
 
 from module.cal_problem_list import cal_problem_list
 from module.gen_problem_list import gen_problem_list
-from module.check_txt import load,check
+from module.check_txt import load, check
+
 
 # 主动抛出异常
 def raise_ex(s1, s2):
@@ -15,55 +16,55 @@ def raise_ex(s1, s2):
     elif (s1 == '-e' and s2 == '-a'):
         return 2
     else:
-     ex = Exception('Error:参数格式错误！')
-     raise ex
+        ex = Exception('Error:参数格式错误！')
+        raise ex
 
 
-def making_problem(n,r):
-            try:
-                int(n)
-                int(r)
-            except ValueError:
-                print("Error:num处输入的不是整数！")
-            else:
-                Exer = open('Exercises' + '.txt', "w",
-                            encoding='utf-8')  ##创建两个txt文件
-                Answ = open('Answers' + '.txt', "w", encoding='utf-8')
-                n = int(n)
-                r = int(r)
-                problem = gen_problem_list(n, r)
-                print(problem)
-                answer = cal_problem_list(problem)
+def making_problem(n, r):
+    try:
+        int(n)
+        int(r)
+    except ValueError:
+        print("Error:num处输入的不是整数！")
+    else:
+        Exer = open('Exercises' + '.txt', "w", encoding='utf-8')  ##创建两个txt文件
+        Answ = open('Answers' + '.txt', "w", encoding='utf-8')
+        n = int(n)
+        r = int(r)
+        problem = gen_problem_list(n, r)
+        print(problem)
+        answer = cal_problem_list(problem)
 
-                for i in range(len(problem)):
-                    p = str(problem[i])
-                    p = str(i + 1) + '. ' + p + ' = ' + '\n'
-                    Exer.write(p)
-                Exer.close()
+        for i in range(len(problem)):
+            p = str(problem[i])
+            p = str(i + 1) + '. ' + p + ' = ' + '\n'
+            Exer.write(p)
+        Exer.close()
 
-                for j in range(len(answer)):
-                    s = str(answer[j])
-                    s = str(j + 1) + '. ' + s + '\n'
-                    Answ.write(s)
-                Answ.close()
-                print(answer)
+        for j in range(len(answer)):
+            s = str(answer[j])
+            s = str(j + 1) + '. ' + s + '\n'
+            Answ.write(s)
+        Answ.close()
+        print(answer)
+
 
 def main():
     try:
         s1, n, s2, r = sys.argv[1:5]
     except BaseException:
-        print("Error: 输入命令错误")# \n正确格式为-n num -r num \n
+        print("Error: 输入命令错误")  # \n正确格式为-n num -r num \n
     else:
         try:
-            res=raise_ex(s1, s2)
+            res = raise_ex(s1, s2)
         except Exception as re:
             print(re)
         else:
-            if (res==1):
-                making_problem(n,r)
+            if (res == 1):
+                making_problem(n, r)
             elif (res == 2):
-                e = os.getcwd() +'\\'+ n
-                a = os.getcwd() +'\\'+ r
+                e = os.getcwd() + '\\' + n
+                a = os.getcwd() + '\\' + r
                 load_e = load(e)
                 load_a = load(a)
                 check(load_e, load_a)
--- module/check_txt.py
+++ module/check_txt.py
@@ -2,38 +2,40 @@ import os
 import re
 from module.cal_problem_list import cal_problem_list
 
+
 def load(path):
- try:
-  load_txt = [w.strip() for w in open(path, 'r', encoding='UTF-8').readlines()]
- except IOError:
-  print("Error: 没有从该路径：{}找到文件/读取文件失败".format(path))
- else:
-  change = []
-  for i in load_txt:
-   input_pro = re.sub(r"(\d+\. )", r'', i)  # 去掉序号
-   input_pro = input_pro.replace("=", '')  # 去掉等号
-   # input_pro = re.sub(r"\s+", "", input_pro)#去掉空格
-   change.append(input_pro)
-  return change
+    try:
+        load_txt = [
+            w.strip() for w in open(path, 'r', encoding='UTF-8').readlines()
+        ]
+    except IOError:
+        print("Error: 没有从该路径：{}找到文件/读取文件失败".format(path))
+    else:
+        change = []
+        for i in load_txt:
+            input_pro = re.sub(r"(\d+\. )", r'', i)  # 去掉序号
+            input_pro = input_pro.replace("=", '')  # 去掉等号
+            # input_pro = re.sub(r"\s+", "", input_pro)#去掉空格
+            change.append(input_pro)
+        return change
 
 
-def check(problem,answer):
- standard_ans=cal_problem_list(problem)
- #默认输入答案对仗
- wrong=[]
- right=[]
- for i in range(len(standard_ans)):
-  if standard_ans[i]!=answer[i]:
-   wrong.append(str(i+1))
-  else:
-   right.append(str(i+1))
- correct_txt ='Correct:'+str(len(right)) +'('+", ".join(str(i) for i in right)+')'
- wrong_txt ='Wrong:' + str(len(wrong)) + '(' + ", ".join(str(i) for i in wrong) + ')'
- Grade= open('Grade' + '.txt', "w", encoding='utf-8')
- Grade.write(correct_txt)
- Grade.write(wrong_txt)
+def check(problem, answer):
+    standard_ans = cal_problem_list(problem)
+    #默认输入答案对仗
+    wrong = []
+    right = []
+    for i in range(len(standard_ans)):
+        if standard_ans[i] != answer[i]:
+            wrong.append(str(i + 1))
+        else:
+            right.append(str(i + 1))
+    correct_txt = 'Correct:' + str(len(right)) + '(' + ", ".join(
+        str(i) for i in right) + ')'
+    wrong_txt = 'Wrong:' + str(len(wrong)) + '(' + ", ".join(
+        str(i) for i in wrong) + ')'
+    Grade = open('Grade' + '.txt', "w", encoding='utf-8')
+    Grade.write(correct_txt)
+    Grade.write(wrong_txt)
 
- return print(correct_txt+'\n'+wrong_txt)
-
-
-
+    return print(correct_txt + '\n' + wrong_txt)
