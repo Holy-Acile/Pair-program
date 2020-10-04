d15bb36c2d5c61ec1ee44ab6699fe84934f849fd
Holy-Acile <huanahuan0202@163.com>
2020-10-05 00:34:17 +0800

    Finish

--- Myapp.py
+++ Myapp.py
@@ -1,25 +1,28 @@
-#all ok!!！
+# all ok!!！
 import os
 import re
 from fractions import Fraction
 import sys
+import time
 
 from module.cal_problem_list import cal_problem_list
 from module.gen_problem_list import gen_problem_list
-from module.check_txt import load,check
+from module.check_txt import load, check
 
 # 主动抛出异常
+
+
 def raise_ex(s1, s2):
     if (s1 == '-n' and s2 == '-r'):
         return 1
     elif (s1 == '-e' and s2 == '-a'):
         return 2
     else:
-     ex = Exception('Error:参数格式错误！')
-     raise ex
+        ex = Exception('Error:参数格式错误！')
+        raise ex
 
 
-def making_problem(n,r):
+def making_problem(n, r):
     try:
         int(n)
         int(r)
@@ -27,7 +30,7 @@ def making_problem(n,r):
         print("Error:num处输入的不是整数！")
     else:
         Exer = open('Exercises' + '.txt', "w",
-                    encoding='utf-8')  ##创建两个txt文件
+                    encoding='utf-8')  # 创建两个txt文件
         Answ = open('Answers' + '.txt', "w", encoding='utf-8')
         n = int(n)
         r = int(r)
@@ -48,27 +51,30 @@ def making_problem(n,r):
         Answ.close()
         print(answer)
 
+
 def main():
+    start_time = time.time()
     try:
         s1, n, s2, r = sys.argv[1:5]
     except BaseException:
-        print("Error: 输入命令错误")# \n正确格式为-n num -r num \n
+        print("Error: 输入命令错误")  # \n正确格式为-n num -r num \n
     else:
         try:
-            res=raise_ex(s1, s2)
+            res = raise_ex(s1, s2)
         except Exception as re:
             print(re)
         else:
-            if (res==1):
-                making_problem(n,r)
+            if (res == 1):
+                making_problem(n, r)
             elif (res == 2):
-                e = os.getcwd() +'\\'+ n
-                a = os.getcwd() +'\\'+ r
+                e = os.getcwd() + '\\' + n
+                a = os.getcwd() + '\\' + r
                 load_e = load(e)
                 load_a = load(a)
                 check(load_e, load_a)
-            # else:
-            #     print('Error:选择错误')
+            end_time = time.time()
+            time_required = end_time - start_time
+            print('程序所耗时间：%f s' % (time_required))
 
 
 if __name__ == '__main__':
