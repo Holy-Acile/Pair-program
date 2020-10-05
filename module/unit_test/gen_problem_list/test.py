1e8e2168e2714060848fee3f0116c94a18be50fd
MrEdge <mredge123@163.com>
2020-10-05 21:23:02 +0800

    test gen_problem_list module

--- module/unit_test/gen_problem_list/test.py
+++ module/unit_test/gen_problem_list/test.py
@@ -1,3 +1,9 @@
 import sys
 sys.path.append("../../../")
 from module.gen_problem_list import gen_problem_list
+
+if __name__ == "__main__":
+    cnt = 10
+    mod = 10
+    problem_list = gen_problem_list(cnt, mod)
+    print(problem_list)
