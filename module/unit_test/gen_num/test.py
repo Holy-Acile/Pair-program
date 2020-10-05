bf7c85b8a4d675f699239efd20fe2505052a4b9e
MrEdge <mredge123@163.com>
2020-10-05 18:59:17 +0800

    test gen_num and gen_problem module

--- module/unit_test/gen_num/test.py
+++ module/unit_test/gen_num/test.py
@@ -1,3 +1,18 @@
 import sys
 sys.path.append("../../../")
 from module.gen_num import gen_num
+
+if __name__ == "__main__":
+    mod = 10
+    ok = True
+
+    for i in range(100) :
+        num = gen_num(10)
+        if num >= mod :
+            ok = False
+            break
+    
+    if ok :
+        print("Finish test!!!")
+    else :
+        print("Error!!!")
--- module/unit_test/gen_problem/test.py
+++ module/unit_test/gen_problem/test.py
@@ -1,3 +1,51 @@
 import sys
 sys.path.append("../../../")
 from module.gen_problem import gen_problem
+from fractions import Fraction
+
+if __name__ == "__main__":
+    for i in range(100) :
+        ok_exp = True
+        ok_val = True
+
+        cmp_val = Fraction(0, 1)
+        mod = 10
+
+        problem = gen_problem(mod)
+
+        for elem in problem :
+            if type(elem) == type(cmp_val) :
+                if elem >= mod :
+                    ok_val = False
+                    break
+
+        exp = ""
+        for elem in problem :
+            if type(elem) == type(cmp_val) :
+                exp += "(" + str(elem) + ")"
+            else :
+                if elem == "×" : 
+                    exp += "*"
+                elif elem == "÷" : 
+                    exp += "/"
+                else :
+                    exp += elem
+        
+        try:
+            eval(exp)
+        except:
+            ok_exp = False
+        finally:
+            if ok_exp and ok_val :
+                pass
+            else :
+                if ok_exp == False :
+                    print("Expression Error!!!")
+                    print(exp)
+                if ok_val == False :
+                    print("Value greater than mod!!!")
+                
+                break
+    
+    if ok_exp and ok_val :
+        print("Finish Test")
