737a494c303cc79f73fbe3e4c0abc6f1ae67f5b2
MrEdge <mredge123@163.com>
2020-10-07 13:20:44 +0800

    modify gen_num rules

--- module/gen_num.py
+++ module/gen_num.py
@@ -7,16 +7,21 @@ def gen_num(mod):
     if mod <= 0: return Fraction(0, 1)
     bit = random.randint(1, 10)
 
+    if mod == 1:
+        down = random.randint(2, 10)
+        up = random.randint(1, down - 1)
+        return Fraction(up, down)
+
     #自然数
     if bit >= 1 and bit <= 5:
         return Fraction(random.randint(0, mod - 1), 1)
     #小于1的真分数
-    elif (bit >= 6 and bit <= 8) or mod == 1:
-        down = random.randint(2, mod + 5)
+    elif (bit >= 6 and bit <= 8):
+        down = random.randint(2, max(5, mod))
         up = random.randint(1, down - 1)
         return Fraction(up, down)
     #带分数
     else:
-        down = random.randint(2, mod + 5)
+        down = random.randint(2, max(5, mod))
         up = random.randint(down, mod * down - 1)
         return Fraction(up, down)
